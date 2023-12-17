9f9e9acbe3ca832e49d1bc35a126e7854a503d6a
xtrm <oss@xtrm.me>
2023-12-17 22:46:28 +0100

    ✨ feature(butterfly): shrink bruteforce range by using linear interpolation
    Signed-off-by: xtrm <oss@xtrm.me>

--- src/push_swap/sorting/butterfly/ps_butterfly_sort.c
+++ src/push_swap/sorting/butterfly/ps_butterfly_sort.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 19:46:08 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/12/17 10:15:15 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/12/17 22:44:38 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,32 @@ static t_list	*ps_butterfly_n_item(t_stack *a, t_stack *b, size_t n_per_boxes)
 	return (list);
 }
 
+static size_t	ps_linear_interpolate_start(size_t size)
+{
+	return ((size_t)(0.06 * ((float)size) + 9));
+}
+
+static size_t	ps_linear_interpolate_end(size_t size)
+{
+	size_t	start;
+	size_t	range;
+
+	start = ps_linear_interpolate_start(size);
+	range = ((size_t)(0.03 * ((float)size) + 32));
+	return (start + range);
+}
+
 t_list	*ps_butterfly_sort(t_stack *a, t_stack *b)
 {
 	t_list	*list;
 	t_list	*tmp;
 	size_t	n_per;
 
-	n_per = a->size / 10;
+	n_per = ps_linear_interpolate_start(a->size);
 	if (n_per == 0)
 		n_per = 1;
 	list = NULL;
-	while (n_per <= (size_t)ft_min(a->size / 2, 90))
+	while (n_per <= ps_linear_interpolate_end(a->size))
 	{
 		tmp = ps_butterfly_n_item(ps_stack_clone(a), ps_stack_clone(b), n_per);
 		if (!tmp)
